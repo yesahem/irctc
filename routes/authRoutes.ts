89a6eb214614897f2bc52100c1e2f989fbeff29b
Shishu Ranjan <ahemraj82@gmail.com>
2025-11-10 01:46:59 +0530

    signup-signin route

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,6 +1,9 @@
 import express, { type Request, type Response } from "express";
-
 import { userLoginInput, userSignupInput } from "../types/types";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { prisma } from "../src";
+import "dotenv/config";
 
 export const authRouter = express.Router();
 
@@ -28,19 +31,110 @@ async function signUp(req: Request, res: Response) {
     return;
   }
 
-  res.json({
-    message: "signup route with valid inputs",
-  });
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 13);
+    console.log("hashedPassword", hashedPassword);
+    const userData = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+        userName: req.body.username,
+        password: hashedPassword,
+      },
+    });
+
+    if (userData.userId != null) {
+      res.json({
+        message: "User added sucessfully",
+        userId: userData.userId,
+      });
+      return;
+    }
+    res.json({
+      message: "Can't create user",
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.json({
+        message: "Can't create user",
+        error: err,
+      });
+    }
+  }
 }
 
 async function signIn(req: Request, res: Response) {
   const { success } = userLoginInput.safeParse(req.body);
+
+  console.log("isSuccess", success);
   if (!success) {
     res.json({
       message: "Invalid Inputs",
     });
     return;
   }
+
+  try {
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            userName: req.body.username,
+          },
+          {
+            email: req.body.email,
+          },
+        ],
+      },
+    });
+
+    if (!existingUser) {
+      res.json({
+        message: "user doesn't exists with the given username",
+      });
+      return;
+    }
+
+    const comparedPassword = await bcrypt.compare(
+      req.body.password,
+      existingUser?.password,
+    );
+
+    if (!comparedPassword) {
+      res.json({
+        message: "entered password is incorrect",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        userId: existingUser.userId,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1h",
+      },
+    );
+
+    res.json({
+      message: "user found",
+      token,
+    });
+    return;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log("error after instance");
+      console.log(err);
+    }
+    res.json({
+      message: "Something went wrong",
+      error: err,
+    });
+    return;
+  }
+
   res.json({
     messgae: "signin Route with valid inputs",
   });
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import express, { type Request, type Response } from "express";
 import { userRouter } from "../routes/userRoutes";
 import { authRouter } from "../routes/authRoutes";
 import { trainRoutes } from "../routes/trainRoutes";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+export const prisma = new PrismaClient();
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
